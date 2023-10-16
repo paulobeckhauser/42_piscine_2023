cd2bf6ad8b1eb2c2322fe6017fa4940fef5bd977
paulo_beckhauser <pauloricardobeckhauser@gmail.com>
2023-10-16 18:11:06 +0200

    Addition of file to write the reverse alphabet

---
+++ c00/ex02/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 17:49:12 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/10/16 18:09:12 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_print_reverse_alphabet(void);
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	ft_putchar('\n');
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
